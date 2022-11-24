d0f2ef7433e7b8b91eef138e581ec4efd6e52f2e
Paulo Marques <paulomarques.rs1@gmail.com>
2022-11-24 02:13:46 -0300

    Inclusão - Comentários notas de uso do ContextAPI

--- src/common/context/User.js
+++ src/common/context/User.js
@@ -1,11 +1,18 @@
 import { createContext, useState } from "react";
 
+// crio o context que vai ser chamado na página (Login) pelo useContext do react onde seus values serão desestruturados
 export const UserContext = createContext();
+// nomeio o context
+UserContext.displayName = 'User';
 
+// crio um provedor para meu context
+// meu provedor recebe um filho como props que será passado quando chamado no arquivo de routes
 export const UserProvider = ({ children }) => {
+    //crio meus estados para meu filho
     const [ name, setName ] = useState('');
     const [ balance, setBalance ] = useState(0);
 
+    // retorno meu provedor com os valores dos estados e a prop filho
     return (
         <UserContext.Provider value={{ name, setName, balance, setBalance }}>
             {children}
