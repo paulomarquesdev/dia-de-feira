873bd2ca04780da6082bfe05ab2cda074c72c28e
Paulo Marques <paulomarques.rs1@gmail.com>
2022-11-30 15:50:25 -0300

    Update - Centralizando login

--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -4,7 +4,9 @@ import FormControl from '@material-ui/core/FormControl';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
   padding: 0 20px;
+  height: 100vh;
   @media(min-width: 768px) {
     margin: 0 auto;
     width: 60%;
@@ -13,7 +15,7 @@ export const Container = styled.div`
 
 export const Titulo = styled.h2`
   margin-bottom: 20px;
-  margin-top: 50px;
+  /* margin-top: 50px; */
 `;
 
 export const InputContainer = styled(FormControl)`
